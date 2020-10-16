464d1341f9fe04677c4beec49ce9cbaf0b05156e
KlausStrohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2020-10-16 17:24:48 +0200

    wip

--- tools/perf-test/main.c
+++ tools/perf-test/main.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <time.h>
 #include <unistd.h>
+#include <inttypes.h>
 
 void error(char const *const s)
 {
@@ -12,12 +13,12 @@ void error(char const *const s)
 
 void send(snd_rawmidi_t *handle, const uint8_t *data, size_t const numBytes)
 {
-  if(handle)
+  if (handle)
   {
     ssize_t res;
-    if(res = snd_rawmidi_write(handle, data, numBytes))
+    if (res = snd_rawmidi_write(handle, data, numBytes))
     {
-      if((size_t) res != numBytes)
+      if ((size_t) res != numBytes)
       {
         error("Could not write message into midi output device");
       }
@@ -26,45 +27,61 @@ void send(snd_rawmidi_t *handle, const uint8_t *data, size_t const numBytes)
   }
 }
 
-static void LogClock(char const *const s)
+static uint64_t getTimeUSec(void)
 {
-  int result;
+  int             result;
   struct timespec tp;
-  clockid_t clk_id;
 
-  clk_id = CLOCK_MONOTONIC;
-
-  result = clock_gettime(clk_id, &tp);
-  printf("MIDI %s TimeStamp %lld:%ld\n", s, tp.tv_sec, tp.tv_nsec);
+  if (clock_gettime(CLOCK_MONOTONIC, &tp))
+    return 0;
+  else
+    return 1000000ul * (uint64_t)(tp.tv_sec) + (uint64_t)(tp.tv_nsec) / 1000ul;
 }
+
+static uint8_t dataOut[300];
+static uint8_t dataIn[sizeof dataOut];
 
 int main(int argc, char *argv[])
 {
-  snd_rawmidi_t *midi1;
-  snd_rawmidi_t *midi2;
+  snd_rawmidi_t *midiOut;
+  snd_rawmidi_t *midiIn;
 
-  if(snd_rawmidi_open(NULL, &midi1, argv[1], SND_RAWMIDI_NONBLOCK))
+  if (snd_rawmidi_open(NULL, &midiOut, argv[1], SND_RAWMIDI_NONBLOCK))
     return 3;
-  if(snd_rawmidi_open(&midi2, NULL, argv[2], SND_RAWMIDI_NONBLOCK))
+  if (snd_rawmidi_open(&midiIn, NULL, argv[2], 0))  // blocking
     return 3;
 
-  uint8_t dataOut[] = { 0x80, 0x30, 0x30, 0x90, 0x30, 0x30 };
-  uint8_t dataIn[sizeof dataOut];
+  dataOut[0]                    = 0xF0;
+  dataOut[(sizeof dataOut) - 1] = 0xF7;
 
-  while(1)
+  uint64_t time;
+  uint64_t min = ~(uint64_t) 0;
+  uint64_t max = 0;
+
+  uint32_t cnt = 0;
+  uint64_t sum = 0;
+
+  while (1)
   {
-    usleep(30);
-    send(midi1, dataOut, sizeof dataOut);
-    // LogClock("write");
-    snd_rawmidi_read(midi2, dataIn, sizeof dataIn);
+    time = getTimeUSec();
+    send(midiOut, dataOut, sizeof dataOut);
+    snd_rawmidi_read(midiIn, dataIn, sizeof dataIn);
+    time = getTimeUSec() - time;
+
+    if (time > max)
+      max = time;
+    if (time < min)
+      min = time;
+    sum += time;
+
+    usleep(1000ul);
+    if (cnt++ % 1000 == 0)
+    {
+      printf("min:%10.3lfms max:%10.3lfms avg:%10.3lfms\n", ((double) min) / 1000.0, ((double) max) / 1000.0, ((double) sum) / 1000.0 / cnt);
+    }
   }
-#if 0
-  LogClock("read ");
-  for(int i = 0; i < sizeof dataIn; i++)
-    printf("%02X ", dataOut[i]);
-  printf("\n");
-#endif
-  snd_rawmidi_close(midi1);
-  snd_rawmidi_close(midi2);
+
+  snd_rawmidi_close(midiOut);
+  snd_rawmidi_close(midiIn);
   return 0;
 }
