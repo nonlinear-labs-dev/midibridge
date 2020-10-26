d02fe5310192fd9e1a2bcc01b671ede1eaed6927
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2020-10-26 17:17:26 +0100

    globals.h : add compiler switch for LED test via SysEx data

--- firmware/src/shared/sys/globals.h
+++ firmware/src/shared/sys/globals.h
@@ -4,3 +4,5 @@
 #define USBA_PORT_FOR_MIDI (0)  // select hardware USB module (USB0 or USB1) for logical port A
 #define USBB_PORT_FOR_MIDI (1)  // select hardware USB module (USB0 or USB1) for logical port B
 // NOTE: of course, the assignments cannot be the same !!
+
+#define LED_TEST (0)
