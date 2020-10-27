5f050c938779eb3e30417e3e11dfef302047017f
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2020-10-27 17:01:27 +0100

    Update nl_midi_isp.c

--- firmware/src/main/src/midi/nl_midi_isp.c
+++ firmware/src/main/src/midi/nl_midi_isp.c
@@ -1,21 +1,27 @@
 #include "midi/nl_midi_isp.h"
 
 // clang-format off
-static const uint8_t ISP_START[] = { // "NLMBISPS"
+// "NLMBISPS"
+// sysex: f0 4e 4c 4d 42 49 53 50 53 f7
+static const uint8_t ISP_START[] = {
   0x04, 0xF0, 'N', 'L',
   0x04,  'M', 'B', 'I',
   0x04,  'S', 'P', 'S',
   0x05, 0xF7,  0,  0
 };
 
-static const uint8_t ISP_END[] = { // "NLMBISPE"
+// "NLMBISPE"
+// sysex: f0 4e 4c 4d 42 49 53 50 45 f7
+static const uint8_t ISP_END[] = {
   0x04, 0xF0, 'N', 'L',
   0x04,  'M', 'B', 'I',
   0x04,  'S', 'P', 'E',
   0x05, 0xF7, 0,   0
 };
 
-static const uint8_t ISP_EXEXCUTE[] = { // "NLMBISPX"
+// "NLMBISPX"
+// sysex: f0 4e 4c 4d 42 49 53 50 58 f7
+static const uint8_t ISP_EXEXCUTE[] = {
   0x04, 0xF0, 'N', 'L',
   0x04,  'M', 'B', 'I',
   0x04,  'S', 'P', 'X',
