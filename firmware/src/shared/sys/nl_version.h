0919550a1d8fb8ab607e3216fcacb8a85a03a5b3
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2021-01-13 11:12:39 +0100

    introduce major and minor firmware version numbers

--- firmware/src/shared/sys/nl_version.h
+++ firmware/src/shared/sys/nl_version.h
@@ -7,17 +7,15 @@
 *******************************************************************************/
 #pragma once
 
-#define SW_VERSION 100
+#define SW_VERSION_MAJOR 1
+#define SW_VERSION_MINOR 2
 
 #define STR_IMPL_(x) #x            //stringify argument
 #define STR(x)       STR_IMPL_(x)  //indirection to expand argument macros
-#if defined CORE_M4
-#define CORE "M4"
-#elif defined CORE_M0
-#define CORE "M0"
-#else
-#error "either CORE_M4 or CORE_M0 must be defined!"
-#endif
 
-static const char VERSION_STRING[]          = "\n\nNLL MIDI Host-to-Host Bridge, LPC4337 Core " CORE ", FIRMWARE VERSION: " STR(SW_VERSION) " \n\n\0\0\0";
-static const char VERSION_STRING_STRIPPED[] = "NLL MIDI Host-to-Host Bridge, Version: " STR(SW_VERSION);
+#define SW_VERSION      \
+  STR(SW_VERSION_MAJOR) \
+  "." STR(SW_VERSION_MINOR)
+
+static const char VERSION_STRING[]          = "\n\nNLL MIDI Host-to-Host Bridge, LPC4337, FIRMWARE VERSION: " SW_VERSION " \n\n\0\0\0";
+static const char VERSION_STRING_STRIPPED[] = "NLL MIDI Host-to-Host Bridge, Version: " SW_VERSION;
