474700c7be98a8078b24b7c878bb1d714e904fc8
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2021-01-13 22:34:51 +0100

    nl_version.h: split version number for X.YZ format

--- firmware/src/shared/sys/nl_version.h
+++ firmware/src/shared/sys/nl_version.h
@@ -8,14 +8,15 @@
 #pragma once
 
 #define SW_VERSION_MAJOR 1
-#define SW_VERSION_MINOR 2
+//                        .
+#define SW_VERSION_MINOR_H 0
+#define SW_VERSION_MINOR_L 2
 
 #define STR_IMPL_(x) #x            //stringify argument
 #define STR(x)       STR_IMPL_(x)  //indirection to expand argument macros
 
 #define SW_VERSION      \
   STR(SW_VERSION_MAJOR) \
-  "." STR(SW_VERSION_MINOR)
+  "." STR(SW_VERSION_MINOR_H) STR(SW_VERSION_MINOR_L)
 
-static const char VERSION_STRING[]          = "\n\nNLL MIDI Host-to-Host Bridge, LPC4337, FIRMWARE VERSION: " SW_VERSION " \n\n\0\0\0";
-static const char VERSION_STRING_STRIPPED[] = "NLL MIDI Host-to-Host Bridge, Version: " SW_VERSION;
+static const char VERSION_STRING[] = "\n\nNLL MIDI Host-to-Host Bridge, LPC4337, FIRMWARE VERSION: " SW_VERSION " \n\n\0\0\0";
