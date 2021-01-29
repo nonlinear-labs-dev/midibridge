9194d0b43870dceab776436517b0a392acc261ef
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2021-01-29 16:47:36 +0100

    Update pins.h
    add final product IO-pin assignment

--- firmware/src/shared/io/pins.h
+++ firmware/src/shared/io/pins.h
@@ -8,6 +8,8 @@
 // --------------- LEDs ----------------
 
 // System RGB-LEDs (those are active-low!)
+#ifdef EVAL_BOARD
+
 #define LED_A_init() GPIO_DIR_OUT(0, 15); SFSP(1, 20) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define LED_A           GPIO_Word(0, 15)
 #define LED_B_init() GPIO_DIR_OUT(0, 12); SFSP(1, 17) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
@@ -22,6 +24,24 @@
 #define LED_F_init() GPIO_DIR_OUT(1,  9); SFSP(1, 6) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define LED_F           GPIO_Word(1,  9)
 
+#else
+
+#define LED_A_init() GPIO_DIR_OUT(2,  9); SFSP(5,  0) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_A           GPIO_Word(2,  9)
+#define LED_B_init() GPIO_DIR_OUT(1,  2); SFSP(1,  9) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_B           GPIO_Word(1,  2)
+#define LED_C_init() GPIO_DIR_OUT(2, 11); SFSP(5,  2) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_C           GPIO_Word(2, 11)
+
+#define LED_D_init() GPIO_DIR_OUT(0, 14); SFSP(2, 10) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_D           GPIO_Word(0, 14)
+#define LED_E_init() GPIO_DIR_OUT(1, 11); SFSP(2, 11) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_E           GPIO_Word(1, 11)
+#define LED_F_init() GPIO_DIR_OUT(1, 10); SFSP(2,  9) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define LED_F           GPIO_Word(1, 10)
+
+#endif
+
 // LED assignments :
 #define LED_RED0    LED_A
 #define LED_GREEN0  LED_B
@@ -32,6 +52,8 @@
 
 //
 // Debug-LEDs (those are active-high)
+#ifdef EVAL_BOARD
+
 #define LED_DBG1_init() GPIO_DIR_OUT(2, 0); SFSP(4, 0) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define LED_DBG1           GPIO_Word(2, 0)
 #define LED_DBG2_init() GPIO_DIR_OUT(2, 3); SFSP(4, 3) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
@@ -39,6 +61,18 @@
 #define LED_DBG3_init() GPIO_DIR_OUT(2, 6); SFSP(4, 4) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define LED_DBG3           GPIO_Word(2, 6)
 
+#else
+
+static uint32_t __dummy_pin__;
+
+#define LED_DBG1_init()
+#define LED_DBG1        __dummy_pin__
+#define LED_DBG2_init()
+#define LED_DBG2        __dummy_pin__
+#define LED_DBG3_init()
+#define LED_DBG3        __dummy_pin__
+
+#endif
 
 static inline void debugPinsInit(void)
 {
@@ -65,10 +99,21 @@ static inline void debugPinsInit(void)
 
 
 // --------------- USB VBUS-Monitors ----------------
+#ifdef EVAL_BOARD
+
 #define pinUSB0_VBUS_init() GPIO_DIR_IN(3, 0); SFSP(6, 1) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define pinUSB0_VBUS          GPIO_Word(3, 0)
 #define pinUSB1_VBUS_init() GPIO_DIR_IN(3, 1); SFSP(6, 2) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
 #define pinUSB1_VBUS          GPIO_Word(3, 1)
+
+#else
+
+#define pinUSB0_VBUS_init() GPIO_DIR_IN(2, 0); SFSP(4, 0) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define pinUSB0_VBUS          GPIO_Word(2, 0)
+#define pinUSB1_VBUS_init() GPIO_DIR_IN(3, 0); SFSP(6, 1) = SFS_EIF + SFS_EIB + SFS_DHS + SFS_EPD + SFS_EPU + 0
+#define pinUSB1_VBUS          GPIO_Word(3, 0)
+
+#endif
 
 static inline void USBPinsInit(void)
 {
