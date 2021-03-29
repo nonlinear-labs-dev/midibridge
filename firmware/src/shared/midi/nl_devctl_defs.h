b65e5a6c66a597f553d51914499ed96626e7d4f5
KlausStrohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2021-03-29 11:02:06 +0200

    Use assigned MIDI SysEx ID

--- firmware/src/shared/midi/nl_devctl_defs.h
+++ firmware/src/shared/midi/nl_devctl_defs.h
@@ -2,12 +2,11 @@
 
 #include <stdint.h>
 
-// three-byte MIDI Manufacturer ID (inofficial, selected to avoid conflicts with taken IDs
+// three-byte MIDI Manufacturer ID
 // see https://www.midi.org/specifications-old/item/manufacturer-id-numbers
 #define MMID0 (0)
-#warning TODO: use assigned number when they arrive
-#define MMID1 (0x4E)  // 'N', will likely become 0x20 or 0x21 with an official ID (0x20++ = european group)
-#define MMID2 (0x4C)  // 'L', will become the next free number assigned for us by midi.org
+#define MMID1 (0x21)
+#define MMID2 (0x58)
 
 #define DEVICEID (0x01)  // only 0x01 == MIDI Bridge, currently
 #define MSGTYPE1 (0x44)  // 'D', D.evice
@@ -21,7 +20,6 @@
 #define CMD_LED_TEST_H (0x01)  // ... for all colors (also for alignment during assembly
 #define CMD_LED_TEST   ((CMD_LED_TEST_H << 8) | CMD_LED_TEST_L)
 
-// Note: we are using the non-official MIDI Manufacturer ID 0x00 0x4E 0x4C (ascii 'NL')
 // The ID is mandatory after each 0xF0 sysex start so that other devices will
 // ignore the sysex properly in case it actually reaches the device. This can happen
 // for example in the fw-uploader which issues an INFO request before it continue
